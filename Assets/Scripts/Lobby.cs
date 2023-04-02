a9981c1b1ea9ae97cbf44fcd75cc596e1205ee4d
malottcc <malottcolin@gmail.com>
2023-04-02 17:00:52 -0400

    Update

--- Assets/Scripts/Lobby.cs
+++ Assets/Scripts/Lobby.cs
@@ -13,16 +13,75 @@ namespace It4080
 {
     public class Lobby : NetworkBehaviour
     {
+
+        public It4080.ConnectedPlayers connectedPlayers;
+
+
+        public Button btnStart;
+        public Button btnKick;
+        private bool isCleared = false;
+
+        void Start()
+        {
+            startedClear();
+            //btnKick.gameObject.SetActive(false);
+
+            btnKick = GameObject.Find("BtnKick").GetComponent<Button>();
+            btnStart = GameObject.Find("BtnStartGame").GetComponent<Button>();
+            btnStart.onClick.AddListener(BtnStartGameOnClick);
+        }
+
+        private void BtnStartGameOnClick()
+        {
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            Debug.Log("");
+            NetworkManager.SceneManager.LoadScene("Game", UnityEngine.SceneManagement.LoadSceneMode.Single);
+        }
+
+        private void startedClear()
+        {
+            if (!isCleared)
+            {
+                connectedPlayers.Clear();
+                isCleared = true;
+            }
+            else
+            {
+                isCleared = false;
+            }
+        }
+
         public override void OnNetworkSpawn()
         {
+            /*
+            if (IsServer)
+            {
+                NetworkHandler.Singleton.allPlayers.OnListChanged += LogNetworkListEvent;
+            }
 
+            btnStart.gameObject.SetActive(IsHost);
+            */
         }
 
-        private PlayerCard AddPlayerCard(ulong clientId)
+        /*private PlayerCard AddPlayerCard(ulong clientId)
         {
-            It4080.PlayerCard newCard = ConnectedPlayers.AddPlayer()
+            //It4080.PlayerCard newCard = ConnectedPlayers.AddPlayer();
         }
-        
-
+        */
+        private void LogNetworkListEvent(NetworkListEvent<It4080.PlayerData> changeEvent)
+        {
+            Debug.Log($"Player data changed:");
+            Debug.Log($"    Change Type:  {changeEvent.Type}");
+            Debug.Log($"    Value:        {changeEvent.Value}");
+            Debug.Log($"        {changeEvent.Value.clientId}");
+            Debug.Log($"        {changeEvent.Value.isReady}");
+            Debug.Log($"    Prev Value:   {changeEvent.PreviousValue}");
+            Debug.Log($"        {changeEvent.PreviousValue.clientId}");
+            Debug.Log($"        {changeEvent.PreviousValue.isReady}");
+        }
     }
 }
